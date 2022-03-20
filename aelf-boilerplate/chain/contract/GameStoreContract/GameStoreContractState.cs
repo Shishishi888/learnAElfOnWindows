d34d9a01970bbe8038c9d3f5aa4a18e9909b034e
ShiYang <Shishishi996@163.com>
2022-03-21 01:02:25 +0800

    删除aelf和aelf-boilerplate项目中的.git

---
+++ aelf-boilerplate/chain/contract/GameStoreContract/GameStoreContractState.cs
@@ -0,0 +1,12 @@
+using AElf.Sdk.CSharp.State;
+
+namespace GameStoreContract
+{
+    /// <summary>
+    /// The state class of the contract, it inherits from the AElf.Sdk.CSharp.State.ContractState type. 
+    /// </summary>
+    public class GameStoreContractState : ContractState
+    {
+        // state definitions go here.
+    }
+}
